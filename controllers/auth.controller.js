b2c2122c93a0fca412c8db16120a865ee37c83c9
Amanda Cuervo <amandacuervo11@gmail.com>
2023-02-11 14:07:41 +0100

    google auth done

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,3 +7,41 @@ module.exports.signup = (req, res, next) => {
     res.render('auth/signup')
 }
 
+module.exports.doSignup = (req, res, next) => {
+    const renderWithErrors = (errors) => {
+        res.render('auth/signup', {
+            user: {
+                email: req.body.email,
+                name: req.body.name
+            },
+            errors
+        })
+    }
+
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(!user) {
+                return User.create(req.body)
+                .then(user => {
+                    res.redirect('/')
+                })   
+            } else {
+                renderWithErrors({ email: 'Email alreaudy in use'})
+            }
+        })
+        .catch(err => {
+            if(err instanceof mongoose.Error.ValidationError) {
+                renderWithErrors(err.errors)
+            } else {
+                next(err)
+            }
+        })
+}
+
+module.exports.doLoginGoogle = (req, res, next) => {
+    login(req, res, next, "google-auth");
+  };
+
+module.exports.login = (req, res, next) => {
+    res.render('auth/login')
+}
---
+++ models/Profile.model.js
@@ -0,0 +1,38 @@
+// const userSchema = new mongoose.Schema(
+// {
+// gender: {
+//     type: String,
+//     required: [true, 'Gender is required']
+// },
+// weight: {
+//     type: Number,
+//     required: [true, 'Weight is required']
+// },
+// height: {
+//     type: Number,
+//     required: [true, 'Height is required']
+// },
+// bday: {
+//     type: Date,
+//     required: [true, 'Date of Birth is required']
+// },
+// activity: {
+//     type: String,
+//     required: [true, 'Activity level is required'],
+//     enum: ["sedentary", "low-active", "active", "very-active"]
+
+// },
+// dietGoal: {
+//     type: String,
+//     required: [true, 'Diet goal is required']
+// },
+// goalWeight: {
+//     type: Number,
+//     required: [true, 'Goal Weight is required']
+    
+// },
+// },
+// {
+// timestamps: true,
+// }
+// );
