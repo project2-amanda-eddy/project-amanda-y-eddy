fc13e8b3014fd5c0ba63a96da12de5fd11eda1e2
Edson <eddyao1990@gmail.com>
2023-02-17 11:59:36 +0100

    updated community

--- controllers/community.controller.js
+++ controllers/community.controller.js
@@ -2,8 +2,12 @@ const Comment = require('../models/Community.model');
 const mongoose = require('mongoose');
 
 module.exports.create = (req, res, next) => {
-
-  res.render('user/community', { comments: comment});
+    Comment.find()
+        .then(comments => {
+            res.render('user/community', { comments });
+        })
+        .catch(err => next(err))
+ 
 }
 
 module.exports.doCreate = (req, res, next) => {
