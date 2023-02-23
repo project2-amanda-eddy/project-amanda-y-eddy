a0efe930991cf38a07a05a2ac190d1da2729e22d
Amanda Cuervo <amandacuervo11@gmail.com>
2023-02-23 19:04:13 +0100

    css in progress

--- controllers/community.controller.js
+++ controllers/community.controller.js
@@ -1,5 +1,6 @@
 const Comment = require('../models/Community.model');
 const mongoose = require('mongoose');
+ 
 
 module.exports.create = (req, res, next) => {
 
@@ -52,3 +53,14 @@ module.exports.delete = (req, res, next) => {
       })
       .catch(err => next(err))
   }
+
+
+  module.exports.comment = (req, res, next) => {
+    Comment.find()
+    .populate('user')
+    .populate('comment')
+    .then(comments => {
+        res.render('user/community', { comments });
+    })
+    .catch(err => next(err))
+}
